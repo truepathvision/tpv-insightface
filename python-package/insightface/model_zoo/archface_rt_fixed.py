160b7dd1a43edd7b5ae0f6fd31a2751687a1b352
Patrick Williamson <patrick@truepathvision.com>
2025-07-11 14:18:54 -0400

    updating

--- python-package/insightface/model_zoo/archface_rt_fixed.py
+++ python-package/insightface/model_zoo/archface_rt_fixed.py
@@ -34,8 +34,7 @@ class ArcFaceRT:
         self._fixed_blobs[batch_size] = np.empty((batch_size, 3, *self.input_size), dtype=np.float32)
 
         inputs, outputs, bindings = self._allocate_buffers(batch_size)
-        fixed_blob = self._fixed_blobs[batch_size]
-        np.copyto(inputs[0].host.reshape(fixed_blob.shape), fixed_blob)
+        inputs[0].host = self._fixed_blobs[batch_size]
 
         cudart.cudaStreamBeginCapture(self.stream, cudart.cudaStreamCaptureMode.cudaStreamCaptureModeGlobal)
         cudart.cudaMemcpyAsync(inputs[0].device, inputs[0].host, inputs[0].nbytes,
@@ -84,18 +83,15 @@ class ArcFaceRT:
 
         if batch_size not in self.graph_cache:
             self._setup_batch(batch_size)
-
+        
+        np.copyto(self._fixed_blobs[batch_size], blob)
         entry = self.graph_cache[batch_size]
-        inputs = entry["inputs"]
-        outputs = entry["outputs"]
         graph_exec = entry["graph_exec"]
-        np.copyto(inputs[0].host.reshape(blob.shape), blob)
-        cudart.cudaMemcpyAsync(inputs[0].device, inputs[0].host, inputs[0].nbytes,
-                       cudart.cudaMemcpyKind.cudaMemcpyHostToDevice, self.stream)
+
         cudart.cudaGraphLaunch(graph_exec, self.stream)
         cudart.cudaStreamSynchronize(self.stream)
         
-        return outputs[0].host.reshape(batch_size, -1)
+        return entry['outputs'][0].host.reshape(batch_size, -1)
 
     def close(self):
         if getattr(self, "_closed", False):
