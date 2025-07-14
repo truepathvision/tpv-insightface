e7401667841150f5d6942c7e6c97dd356f54fbed
Patrick Williamson <patrick@truepathvision.com>
2025-07-14 13:46:59 -0400

    updating

--- python-package/insightface/model_zoo/scrfd_graph.py
+++ python-package/insightface/model_zoo/scrfd_graph.py
@@ -228,7 +228,7 @@ class SCRFD_TRT_G:
             cudart.cudaGraphLaunch(self.graph_exec, self.stream)
             cudart.cudaStreamSynchronize(self.stream)
 
-        results = [out.host for out in self.outputs]
+        results = [out.host.copy() for out in self.outputs]
         input_shape = (self._fixed_blob.shape[2], self._fixed_blob.shape[3])
         bboxes, kpss = postprocess_trt_outputs(results, input_shape, threshold=self.threshold)
         bboxes[:, :4] /= scale
